c3ad6c427a7d1361432f28797639ceec70d396e6
vasasharath <sharathvasa@gmail.com>
2019-01-02 16:58:03 -0500

    fixed untracked files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientId: '280306459508754',
-        clientSecret: 'eef5aecb8591faabc35ee4841b7d127f'
-    }
-}

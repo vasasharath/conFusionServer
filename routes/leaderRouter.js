b3a73c8ef8a9809f825af39119684f695e2a0dcc
vasasharath <sharathvasa@gmail.com>
2019-01-02 11:34:20 -0500

    Cors

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -4,83 +4,86 @@ const mongoose = require('mongoose');
 var authenticate = require('../authenticate');
 
 const Leaders = require('../models//leaders');
+const cors = require('./cors');
 
 const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-    .get((req, res, next) => {
-        Leaders.find({})
-            .then((Leaders) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(Leaders);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    })
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req, res, next) => {
+    Leaders.find({})
+    .then((Leaders) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(Leaders);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
 
-    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        Leaders.create(req.body)
-            .then((leader) => {
-                console.log('leader Created ', leader);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(leader);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    })
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.create(req.body)
+    .then((leader) => {
+        console.log('leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
 
-    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        res.statusCode = 403;
-        res.end('Put operation not supported on /Leaders');
-    })
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on /Leaders');
+})
 
-    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        Leaders.remove({})
-            .then((resp) => {
-                res.statusCode = 200,
-                    res.setHeader('Content-Type', 'application/json');
-                res.json(resp);
-            }, (err) => next(err));
-    });
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.remove({})
+    .then((resp) => {
+        res.statusCode = 200,
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err));
+});
 
 leaderRouter.route('/:leaderId')
-    .get((req, res, next) => {
-        Leaders.findById(req.params.leaderId)
-            .then((leader) => {
-                res.statusCode = 200,
-                    res.setHeader('Content-Type', 'application/json');
-                res.json(leader);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    })
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req, res, next) => {
+    Leaders.findById(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode = 200,
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
 
-    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        res.statusCode = 403;
-        res.end('POST operation not supported on /Leaders/' + req.params.leaderId);
-    })
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /Leaders/' + req.params.leaderId);
+})
 
-    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        Leaders.findByIdAndUpdate(req.params.leaderId, {
-            $set: req.body
-        }, { new: true })
-            .then((dish) => {
-                res.statusCode = 200,
-                    res.setHeader('Content-Type', 'appliation/json');
-                res.json(dish);
-            }, (err) => next(err))
-            .catch((err) => next(err));
-    })
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, {new: true})
+    .then((dish) => {
+        res.statusCode = 200,
+        res.setHeader('Content-Type', 'appliation/json');
+        res.json(dish);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
 
-    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-        Leaders.findByIdAndRemove(req.params.leaderId)
-            .then((resp) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(resp);
-            }, (err) => next(err))
-            .catch9((err) => next(err));
-    })
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    Leaders.findByIdAndRemove(req.params.leaderId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch9((err) => next(err));
+})
 
 module.exports = leaderRouter;
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authenticate = require('../authenticate');
 const multer = require('multer');
+const cors  = require('./cors');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/images');
     },
-
     filename: (req, file, cb) => {
-        cb(null, file.originalname)
+        cb(null, file.originalname);
     }
 });
 
@@ -21,26 +21,26 @@ const imageFileFilter = (req, file, cb) => {
 };
 
 const upload = multer({ storage: storage, fileFilter: imageFileFilter});
-
 const uploadRouter = express.Router();
 
 uploadRouter.use(bodyParser.json());
 
 uploadRouter.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('GET operation not supported on /imageUpload');
 })
-.post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.json(req.file);
 })
-.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /imageUpload');
 })
-.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('DELETE operation not supported on /imageUpload');
 });

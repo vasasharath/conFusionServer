9d641e340c343d0a2dda190fc217cf63ecbddf01
vasasharath <sharathvasa@gmail.com>
2019-01-02 14:29:12 -0500

    Assignment 4

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ var authenticate = require('./authenticate');
 var config = require('./config');
 
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favouriteRouter = require('./routes/favouriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -69,6 +71,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites', favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
